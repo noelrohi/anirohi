4180fc0c55f5c00068a959ed82686fcfc55472c6
gneiru <noelrohi59@gmail.com>
2023-08-13 16:15:18 +0800

    add suspense boundary for pages

---
+++ src/app/(stickyHeader)/anime/[slug]/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function SlugLoading() {
+  return (
+    <div className="container">
+      <div className="flex justify-center items-center min-h-screen">
+        Loading ...
+      </div>
+    </div>
+  );
+}
---
+++ src/app/(stickyHeader)/home/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HomeLoading() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      Loading ...
+    </div>
+  );
+}
