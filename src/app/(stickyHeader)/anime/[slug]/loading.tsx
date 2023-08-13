430eeef10b2c1b1eb00edbf8e1a0d3600a6b362c
gneiru <noelrohi59@gmail.com>
2023-08-13 19:03:23 +0800

    exact screen size

--- src/app/(stickyHeader)/anime/[slug]/loading.tsx
+++ src/app/(stickyHeader)/anime/[slug]/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function SlugLoading() {
   return (
     <div className="container">
-      <div className="flex justify-center items-center min-h-screen">
+      <div className="flex justify-center items-center min-h-[calc(100vh-104px-64px)]">
         Loading ...
       </div>
     </div>
--- src/app/(stickyHeader)/home/loading.tsx
+++ src/app/(stickyHeader)/home/loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function HomeLoading() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="flex justify-center items-center min-h-[calc(100vh-104px-64px)]">
       Loading ...
     </div>
   );
