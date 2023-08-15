0fb0354ce26cd585701540e165ba1c52b8945fe6
gneiru <noelrohi59@gmail.com>
2023-08-15 09:37:52 +0800

    fix: padding of siteheader in lobby page

--- src/app/(lobby)/layout.tsx
+++ src/app/(lobby)/layout.tsx
@@ -8,9 +8,7 @@ interface LobbyLayoutProps {
 export default async function LobbyLayout({ children }: LobbyLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
-      <header className="container z-40 bg-background">
-        <SiteHeader />
-      </header>
+      <SiteHeader />
       <main className="flex-1">{children}</main>
       <SiteFooter />
     </div>
