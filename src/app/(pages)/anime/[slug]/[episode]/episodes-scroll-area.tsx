7f24a9ef688e9a7b1be7cb3c1dfd6db4f0c475af
gneiru <noelrohi59@gmail.com>
2023-08-18 16:52:58 +0800

    scroll into current ep in ep list

---
+++ src/app/(pages)/anime/[slug]/[episode]/episodes-scroll-area.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { Icons } from "@/components/icons";
+import { Badge } from "@/components/ui/badge";
+import { ScrollArea, ScrollViewport } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+import { useMounted } from "@/hooks/use-mounted";
+import { cn } from "@/lib/utils";
+import { AnimeResponse } from "@/types/enime";
+import Link from "next/link";
+import { useRef } from "react";
+
+interface EpisodeScrollAreaProps extends React.HTMLProps<HTMLDivElement> {
+  slug: string;
+  currentEpisode: number;
+  episodes: AnimeResponse["episodes"];
+}
+
+export function EpisodeScrollArea({
+  episodes,
+  slug,
+  currentEpisode,
+}: EpisodeScrollAreaProps) {
+  const targetRef = useRef<HTMLDivElement>(null);
+  const scrollableRef = useRef<HTMLDivElement>(null);
+  const mounted = useMounted();
+  if (mounted) {
+    console.log("mounted", mounted);
+    if (scrollableRef.current && targetRef.current) {
+      targetRef.current.scrollIntoView({ behavior: "smooth", block: "center" });
+    }
+  }
+  return (
+    <ScrollArea className="h-[10rem] lg:h-[32rem] xl:h-[37rem] w-full rounded-md border">
+      <ScrollViewport className="p-4" ref={scrollableRef}>
+        {episodes.map((ep) => (
+          <div
+            ref={ep.number === currentEpisode ? targetRef : null}
+            key={ep.id}
+          >
+            <Link
+              href={`/anime/${slug}/${ep.number}`}
+              className="flex flex-row justify-between items-center"
+            >
+              <div className="flex flex-row items-center">
+                <Badge
+                  className={cn(
+                    "px-1 py-0 rounded-full mr-2",
+                    ep.number === currentEpisode &&
+                      "bg-blue-700 dark:bg-blue-400"
+                  )}
+                  variant={"destructive"}
+                >
+                  {ep.number}
+                </Badge>
+                <span
+                  className={cn(
+                    "inline-flex gap-2 items-center justify-between",
+                    ep.number === currentEpisode && "font-bold"
+                  )}
+                >
+                  {ep.title || `Episode ${ep.number}`}
+                </span>
+              </div>
+              {ep.number === currentEpisode && <Icons.check />}
+            </Link>
+            <Separator className="my-2" />
+          </div>
+        ))}
+      </ScrollViewport>
+    </ScrollArea>
+  );
+}
--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area"
+import * as React from "react";
+import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
@@ -14,14 +14,26 @@ const ScrollArea = React.forwardRef<
     className={cn("relative overflow-hidden", className)}
     {...props}
   >
-    <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
+    <ScrollViewport className="h-full w-full rounded-[inherit]">
       {children}
-    </ScrollAreaPrimitive.Viewport>
+    </ScrollViewport>
     <ScrollBar />
     <ScrollAreaPrimitive.Corner />
   </ScrollAreaPrimitive.Root>
-))
-ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName
+));
+ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName;
+
+const ScrollViewport = React.forwardRef<
+  React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaViewport>,
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaViewport>
+>(({ className, ...props }, ref) => (
+  <ScrollAreaPrimitive.ScrollAreaViewport
+    ref={ref}
+    className={cn("h-full w-full rounded-[inherit]", className)}
+    {...props}
+  />
+));
+ScrollViewport.displayName = ScrollAreaPrimitive.ScrollAreaViewport.displayName;
 
 const ScrollBar = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>,
@@ -42,7 +54,7 @@ const ScrollBar = React.forwardRef<
   >
     <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
-))
-ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
+));
+ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
 
-export { ScrollArea, ScrollBar }
+export { ScrollArea, ScrollBar, ScrollViewport };
