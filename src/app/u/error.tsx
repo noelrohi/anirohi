a946c1e24dc9e609d95945111f4b2bfa3b1a1030
gneiru <noelrohi59@gmail.com>
2023-08-26 10:57:48 +0800

    Add error boundary

---
+++ src/app/u/error.tsx
@@ -0,0 +1,33 @@
+"use client"; // Error components must be Client Components
+
+import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen gap-8">
+      <h2 className="font-bold leading-tight text-3xl">
+        Oops! {error.message}
+      </h2>
+      <Button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </Button>
+    </div>
+  );
+}
