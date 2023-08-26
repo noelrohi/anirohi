606497fe13fb51e83260d667ae0c5679ad40e31b
gneiru <noelrohi59@gmail.com>
2023-08-26 11:19:00 +0800

    go home button

--- src/app/u/error.tsx
+++ src/app/u/error.tsx
@@ -1,7 +1,8 @@
 "use client"; // Error components must be Client Components
 
 import { Button } from "@/components/ui/button";
-import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useTransition } from "react";
 
 export default function Error({
   error,
@@ -14,20 +15,35 @@ export default function Error({
     // Log the error to an error reporting service
     console.error(error);
   }, [error]);
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
 
   return (
     <div className="flex flex-col justify-center items-center h-screen gap-8">
       <h2 className="font-bold leading-tight text-3xl">
         Oops! {error.message}
       </h2>
-      <Button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </Button>
+      <div className="flex flex-row gap-4">
+        <Button
+          onClick={
+            // Attempt to recover by trying to re-render the segment
+            () => reset()
+          }
+        >
+          Try again
+        </Button>
+        <Button
+          disabled={isPending}
+          variant={"secondary"}
+          onClick={() => {
+            startTransition(() => {
+              router.push("/home");
+            });
+          }}
+        >
+          {isPending ? "Going" : "Go "} to Home
+        </Button>
+      </div>
     </div>
   );
 }
