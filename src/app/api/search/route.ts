329a79233883fe41af1710632b9774cd757f6e67
gneiru <noelrohi59@gmail.com>
2023-09-18 16:58:05 +0800

    fix: api search

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,21 +1,22 @@
-import { searchAnime } from "@/lib/enime";
-import { getTitle } from "@/lib/utils";
+import { anime } from "@/lib/jikan";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const query = searchParams.get("q");
   if (!query) return NextResponse.json("Query is required!", { status: 400 });
-  const [settled] = await Promise.allSettled([searchAnime(query)]);
+  const [settled] = await Promise.allSettled([
+    anime.getAnimeSearch({ q: query }),
+  ]);
   if (settled.status === "rejected")
     return NextResponse.json([], { status: 200 });
   return NextResponse.json(
-    settled.value.data.map(({ title, slug, year, coverImage }) => {
+    settled.value.data.map(({ title, mal_id, year, images }) => {
       const data = {
-        title: getTitle(title),
-        slug: slug,
+        title: title,
+        slug: mal_id,
         year: year,
-        image: coverImage,
+        image: images.jpg.image_url,
       };
       return data;
     }),
