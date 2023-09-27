abd36fafb552d1d228610c54daa166ad5fb95a4d
gneiru <noelrohi59@gmail.com>
2023-09-27 09:46:57 +0800

    add staff list

--- src/components/staff-toolbar.tsx
+++ src/components/staff-toolbar.tsx
@@ -1,8 +1,10 @@
 import { getServerSession } from '@/lib/nextauth/session';
 import { VercelToolbar } from '@vercel/toolbar/next';
  
+const staffs = ['nrohi']
+
 export async function StaffToolbar() {
   const session = await getServerSession()
-  const isStaff = !!session;
+  const isStaff = session?.name ? staffs.includes(session.name) : false;
   return isStaff ? <VercelToolbar /> : null;
 }
