cd7db0ed3dc8ffb9ff76eb6ba46e690f5bd052c4
gneiru <noelrohi59@gmail.com>
2023-10-21 13:31:04 +0800

    notFound, user Avatar and name heading

--- src/app/u/[name]/page.tsx
+++ src/app/u/[name]/page.tsx
@@ -1,14 +1,14 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { db } from "@/db";
 import { users } from "@/db/schema/auth";
 import { getStats } from "@/lib/anilist";
-import { AreaChart } from "@tremor/react";
+import { absoluteUrl } from "@/lib/utils";
+import { GeneralStats as GenStat } from "@/types/anilist/general-stats";
 import { eq } from "drizzle-orm";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
 import { Suspense } from "react";
 import { Charts, GeneralStats } from "./statistics";
-import { Separator } from "@/components/ui/separator";
-import { Metadata } from "next";
-import { GeneralStats as GenStat } from "@/types/anilist/general-stats";
-import { absoluteUrl } from "@/lib/utils";
 
 interface DashboardPageProps {
   params: {
@@ -26,10 +26,19 @@ interface DashboardPageProps {
 //   return params;
 // }
 
+async function getUserByUsername(username: string) {
+  const data = await db.query.users.findFirst({
+    where: eq(users.name, username),
+  });
+  if (!data) notFound();
+  return data;
+}
+
 export async function generateMetadata({ params }: DashboardPageProps) {
-  const data: GenStat = await getStats(params.name, "general");
-  const title = `${params.name}'s Dashboard`;
-  const description = `A user of Anirohi. Watch anime ${data.data.User.statistics.anime.count} times`;
+  const user = await getUserByUsername(params.name);
+  const data: GenStat = await getStats(user.name, "general");
+  const title = `${user.name}'s Dashboard`;
+  const description = `A user of Anirohi. Watched anime ${data.data.User.statistics.anime.count} times`;
 
   const metadata: Metadata = {
     title,
@@ -57,13 +66,25 @@ export async function generateMetadata({ params }: DashboardPageProps) {
 }
 
 export default async function DashboardPage({ params }: DashboardPageProps) {
-  const user = await db.query.users.findFirst({
-    where: eq(users.name, params.name),
-  });
-  if (!user) throw new Error("User not found! Try signing in...");
+  const user = await getUserByUsername(params.name);
   return (
     <main className="container">
       <div className="flex flex-col gap-8 py-8">
+        <div className="flex gap-4 items-center">
+          <Avatar className="h-20 w-20">
+            <AvatarImage
+              src={user.image ?? ""}
+              alt={user.name ?? ""}
+              className="object-cover"
+            />
+            <AvatarFallback className="capitalize">
+              {user.name?.at(0) ?? "G"}
+            </AvatarFallback>
+          </Avatar>
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            {user.name}
+          </h1>
+        </div>
         <Suspense fallback={<div>Loading cards...</div>}>
           <GeneralStats username={user.name!} />
         </Suspense>
