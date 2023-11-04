6c9d7f5fe4c65bcfcce20b47cb94ae8a4d3df257
gneiru <noelrohi59@gmail.com>
2023-11-04 10:03:01 +0800

    change site descripton as of next14

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,7 +3,7 @@ import { SiteConfig } from "@/types";
 export const siteConfig: SiteConfig = {
   name: "Anirohi",
   description:
-    "An anime streaming app that lets you watch anime online. It's built using Next.js 13, server components and everything new in the new router.",
+    "An anime streaming app that lets you watch anime online without ads. It's built using Next.js 14, Next-Auth and Shadcn-UI.",
   url: "https://ani.rohi.dev",
   links: {
     twitter: "https://twitter.com/gneiru",
