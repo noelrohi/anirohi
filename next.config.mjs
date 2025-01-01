94021e46f31681f61f0ea70023f05b6dd1944ee3
gneiru <noelrohi59@gmail.com>
2025-01-01 12:52:56 +0800

    feat: remove next-pwa, use PWA from nextjs docs

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,16 @@
 import "./src/env.mjs";
-import nextPwa from "next-pwa";
-import toolbar from "@vercel/toolbar/plugins/next"
 
-const withPWA = nextPwa({
-  dest: "public",
-});
- 
 /** @type {import("next").NextConfig} */
 const config = {
   /** ... */
   logging: {
     fetches: {
       fullUrl: true,
-    }
+    },
   },
   images: {
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: "s4.anilist.co",
-
-        },
-        {
-            protocol: 'https',
-            hostname: "artworks.thetvdb.com",
-
-        }
-    ],
     unoptimized: true,
   },
 };
-const withVercelToolbar = toolbar();
- 
-export default process.env.NODE_ENV === "development" ? withVercelToolbar(config) : withVercelToolbar(withPWA(config));
+
+export default config;
