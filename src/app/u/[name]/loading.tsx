db862286fc9d9e8373831fda2300ec7a8b8b6cc3
Rohi <noelrohi59@gmail.com>
2025-12-12 17:57:22 +0800

    rm all

--- src/app/u/[name]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Icons } from "@/components/icons";
-
-export default function DashboardLoading() {
-  return (
-    <div className="flex min-h-[calc(100vh-104px-64px)] items-center justify-center">
-      <Icons.loader className="size-10 animate-spin" />
-    </div>
-  );
-}
